5f337e64b6d34973ccf15915fea990a76d4c6116
ninjafiveo <mjsekol@live.com>
2024-08-26 09:24:05 -0400

    Updated Datatypes

--- datatypes.py
+++ datatypes.py
@@ -7,3 +7,11 @@ first_name = "Michael" # Strings have to be inside of " double quotes" or 'singl
 print(first_name)
 
 # Side note: the assignment operator & variables. "=" is the assignment operator. It is a command that says the variable name is equal to the value. So first_name = "Michael", is a command telling first_name is "Michael"
+
+# int --> Integers. These are whole numbers: -5, -4, -3, -1, 0, 1, 2, 3, 4... 506,322
+user_age = 99
+print(user_age)
+
+# floats --> These are numbers with decimal points. 97.6, 101.2
+patient_temp = 97.6
+print(patient_temp)
